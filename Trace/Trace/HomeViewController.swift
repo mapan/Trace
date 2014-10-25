d68a453b489ce12eec747e6d7a77fe3803ad8651
Kabir Gupta <kabirgup@usc.edu>
2014-10-25 14:41:06 -0700

    Table View methods

--- Trace/Trace/HomeViewController.swift
+++ Trace/Trace/HomeViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel.text = "test"
+        
+        return cell
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> NSInteger {
+        return 1;
     }
 }
